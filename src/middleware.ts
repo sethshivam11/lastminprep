9d5038caa8a58e0ffc0e57979708aaca6e8bf29c
Shivam Soni <130232913+sethshivam11@users.noreply.github.com>
2025-06-18 10:03:56 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const secret = process.env.NEXTAUTH_SECRET;
+  console.log(secret);
   const token = await getToken({ req, secret });
   if (token && req.nextUrl.pathname.includes("/auth")) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
